8d76db18b15659709edf225c7eb988251fb2fe28
chenbaolin <505951628@qq.com>
2017-05-01 22:14:23 +0800

    修改Activity栈管理为软引用

--- app/src/main/java/com/base/util/ActivityCollector.java
+++ app/src/main/java/com/base/util/ActivityCollector.java
@@ -1,17 +1,22 @@
 package com.base.util;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Context;
+import android.util.Log;
 
+import java.lang.ref.WeakReference;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Stack;
 
 /**
  * Created by chenbaolin on 2017/4/15.
+ *
  */
 
 public class ActivityCollector {
-    public  static Stack<Activity> activityStack;
+    private static final String TAG = "ActivityCollector";
+    public static Stack<WeakReference<Activity>> activityStack;
     private static ActivityCollector activityCollector;
 
     private ActivityCollector() {
@@ -19,62 +24,147 @@ public class ActivityCollector {
 
     //获取单例
     public static ActivityCollector getInstance() {
-        if (activityCollector == null) {
-            synchronized (ActivityCollector.class) {
-                if (activityCollector == null) {
-                    activityCollector = new ActivityCollector();
-                }
+        synchronized (ActivityCollector.class) {
+            if (activityCollector == null) {
+                activityCollector = new ActivityCollector();
             }
         }
         return activityCollector;
 
     }
 
+    /***
+     * 栈中Activity的数
+     *
+     * @return Activity的数
+     */
+    public int stackSize() {
+        return activityStack.size();
+    }
+
+    /***
+     * 获得Activity栈
+     *
+     * @return Activity栈
+     */
+    public Stack<WeakReference<Activity>> getStack() {
+        return activityStack;
+    }
+
     /**
      * 添加Activity到堆栈
      */
-    public void addActivity(Activity activity) {
+    public void addActivity(WeakReference<Activity> activity) {
         if (activityStack == null) {
             activityStack = new Stack<>();
         }
         activityStack.add(activity);
     }
 
-    /**
-     * 获取当前Activity（堆栈中最后一个压入的）
+    /***
+     * 获取栈顶Activity（堆栈中最后一个压入的）
+     *
+     * @return Activity
      */
-    public Activity getCurrentActivity() {
-        return activityStack.lastElement();
-    }
-
-    /**
-     * 结束当前Activity（堆栈中最后一个压入的）
-     */
-    public void finishActivity() {
-        Activity activity = activityStack.lastElement();
-        if (activity != null) {
-            activity.finish();
+    public Activity getTopActivity() {
+        Activity activity = activityStack.lastElement().get();
+        if (null == activity) {
+            return null;
+        } else {
+            return activityStack.lastElement().get();
         }
     }
 
+    /***
+     * 通过class 获取栈顶Activity
+     *
+     * @param cls
+     * @return Activity
+     */
+    public Activity getActivityByClass(Class<?> cls) {
+        Activity return_activity = null;
+        for (WeakReference<Activity> activity : activityStack) {
+            if (activity.get().getClass().equals(cls)) {
+                return_activity = activity.get();
+                break;
+            }
+        }
+        return return_activity;
+    }
+
     /**
+     * 结束栈顶Activity（堆栈中最后一个压入的）
+     */
+    public void finishTopActivity() {
+        try {
+            WeakReference<Activity> activity = activityStack.lastElement();
+            killActivity(activity);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    /***
      * 结束指定的Activity
+     *
+     * @param activity
      */
-    public void finishActivity(Activity activity) {
-        if (activity != null) {
-            activityStack.remove(activity);
-            activity.finish();
+    public void killActivity(WeakReference<Activity> activity) {
+        try {
+            Iterator<WeakReference<Activity>> iterator = activityStack.iterator();
+            while (iterator.hasNext()) {
+                WeakReference<Activity> stackActivity = iterator.next();
+                if (stackActivity.get() == null) {
+                    iterator.remove();
+                    continue;
+                }
+                if (stackActivity.get().getClass().getName().equals(activity.get().getClass().getName())) {
+                    iterator.remove();
+                    stackActivity.get().finish();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
     }
 
     /**
+     * 删除ac
+     *
+     * @param activity 弱引用的ac
+     */
+    public void removeActivity(WeakReference<Activity> activity) {
+        if (activityStack != null) {
+            activityStack.remove(activity);
+        }
+    }
+
+    /***
      * 结束指定类名的Activity
+     *
+     * @param cls
      */
     public void finishActivity(Class<?> cls) {
-        for (Activity activity : activityStack) {
-            if (activity.getClass().equals(cls)) {
-                finishActivity(activity);
+        try {
+
+            ListIterator<WeakReference<Activity>> listIterator = activityStack.listIterator();
+            while (listIterator.hasNext()) {
+                Activity activity = listIterator.next().get();
+                if (activity == null) {
+                    listIterator.remove();
+                    continue;
+                }
+                if (activity.getClass() == cls) {
+                    listIterator.remove();
+                    if (activity != null) {
+                        activity.finish();
+                    }
+                    break;
+                }
             }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
     }
 
@@ -82,12 +172,39 @@ public class ActivityCollector {
      * 结束所有Activity
      */
     public void finishAllActivity() {
-        for (Activity activity : activityStack) {
-            if (activity != null) {
-                activity.finish();
+        try {
+            ListIterator<WeakReference<Activity>> listIterator = activityStack.listIterator();
+            while (listIterator.hasNext()) {
+                Activity activity = listIterator.next().get();
+                if (activity != null) {
+                    activity.finish();
+                }
+                listIterator.remove();
             }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
-        activityStack.clear();
+    }
+
+    /**
+     * 移除除了某个activity的其他所有activity
+     *
+     * @param cls 界面
+     */
+    public void killAllActivityExceptOne(Class cls) {
+        try {
+            for (int i = 0; i < activityStack.size(); i++) {
+                WeakReference<Activity> activity = activityStack.get(i);
+                if (activity.getClass().equals(cls)) {
+                    break;
+                }
+                if (activityStack.get(i) != null) {
+                    killActivity(activity);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 
     /**
@@ -96,13 +213,13 @@ public class ActivityCollector {
     public void AppExit(Context context) {
         try {
             finishAllActivity();
+            // 杀死该应用进程
+            android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(0);
-            ActivityManager manager = (ActivityManager) context
-                    .getSystemService(Context.ACTIVITY_SERVICE);
-            manager.killBackgroundProcesses(context.getPackageName());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 
 }
